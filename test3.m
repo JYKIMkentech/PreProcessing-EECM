43bd41d676735c4e030b85373bc4c2b86009252a
JYKIMkentech <deu04152@kentech.ac.kr>
2024-02-29 17:58:54 +0900

    Update test3.m

--- test3.m
+++ test3.m
@@ -2,6 +2,9 @@ clear; clc; close all;
 
 I_1C = 0.00429;
 
+%RPT 순서 : OCV - DCIR - Crate - GITT
+
+
 load('G:\공유 드라이브\BSL_Data2\HNE_AgingDOE_Processed\HNE_FCC\4CPD 1C (25-42)\25degC\HNE_FCC_4CPD 1C (25-42)_25degC_s01_6_6_RPT1.mat'); %RPT_file
 
 data1.t = vertcat(data(1:end).t);
@@ -19,6 +22,7 @@ plot(data1.t/3600,data1.I/I_1C)
 yyaxis right
 ylabel('current (C)')
 
-
-
+data_ocv = data(1:6); % OCV = 1부터 6번째 struct 
+data_dcir = data(7:164);
+% data_rate = data(165: )
 
