fb45edcaa66a34656e2e539ed404d3123f3354d1
JYKIMkentech <129360396+JYKIMkentech@users.noreply.github.com>
2024-04-10 15:12:54 +0900

    .
    .

--- AHC_rate.m
+++ AHC_rate.m
@@ -1,25 +1,12 @@
 clc;clear;close all
 
-data_folder = 'G:\공유 드라이브\BSL-Data\Processed_data\Hyundai_dataset\RPT_GITT제외\HNE_AHC_(3)_RPT\[HNE_AHC_RPT_01_043].mat';
-%OCV_fullpath = 'G:\공유 드라이브\BSL-Data\Processed_data\Hyundai_dataset\OCV_test\CHC\25deg\[HNE_CHC_04_OCV_C20_25deg_016].mat';
-
-load(data_folder);
-
-I_1C = 0.0043;
-
-data1.I = vertcat(data.I);
-data1.V = vertcat(data.V);
-data1.t = vertcat(data.t);
-
-figure
-plot(data1.t/3600, data1.V, '-')
-xlabel('time (hours)')
-ylabel('voltage (V)')
-yyaxis right
-plot(data1.t/3600, data1.I/I_1C, '-')
-ylabel('current (C)')
-
+%% WORK FLOW
 
 % AHC에서 역 C-RATE --> X,V 가져오기
 % CHC에서 C-RATE ---> Y,V 가져오기
 
+
+OCP_data = 'G:\공유 드라이브\BSL-Data\Data\Hyundai_dataset\RPT_data(Formation,OCV,DCIR,C-rate,GITT,RPT)\OCV_data\OCV2\HNE_(5)_AHC_OCV2.mat'; % OCP AHC 가져오기
+%OCV_fullpath = 'G:\공유 드라이브\BSL-Data\Processed_data\Hyundai_dataset\OCV_test\CHC\25deg\[HNE_CHC_04_OCV_C20_25deg_016].mat';
+
+OCP_data = load(OCP_data);
